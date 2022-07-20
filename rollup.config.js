ede4615a612d5956f3b0e039d3ef2367bc0993e1
Andrey Alekseev <andrey.a@prpl.global>
2022-07-20 15:38:18 +0300

    add tests

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,9 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from 'rollup-plugin-postcss';
 
+import {terser} from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 const packageJson = require("./package.json");
 
 export default [
@@ -13,24 +16,26 @@ export default [
       {
         file: packageJson.main,
         format: "cjs",
-        sourcemap: true,
+        sourcemap: false,
       },
       {
         file: packageJson.module,
         format: "esm",
-        sourcemap: true,
+        sourcemap: false,
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({tsconfig: "./tsconfig.json"}),
       postcss(),
+      terser()
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    output: [{file: "dist/index.d.ts", format: "esm"}],
     plugins: [dts()],
     external: [/\.css$/],
   },
